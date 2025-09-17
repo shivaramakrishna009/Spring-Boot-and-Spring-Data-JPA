2b2811ef1b0453d716968e7f3a332b5cc9e5e4c8
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-09-17 06:53:52 +0530

    Add MyFirstClass and integrate greeting functionality in main application

---
+++ spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/MyFirstClass.java
@@ -0,0 +1,8 @@
+package com.krishnaallu009.springBoot;
+
+public class MyFirstClass {
+
+    public String greet() {
+        return "Hello, World!";
+    }
+}
--- spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/SpringBootAndDataJpaApplication.java
+++ spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/SpringBootAndDataJpaApplication.java
@@ -2,12 +2,21 @@ package com.krishnaallu009.springBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootAndDataJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAndDataJpaApplication.class, args);
+		var ctx = SpringApplication.run(SpringBootAndDataJpaApplication.class, args);
+
+        MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class);
+        System.out.println(myFirstClass.greet());
 	}
+
+    @Bean
+    public MyFirstClass myFirstClass() {
+        return new MyFirstClass();
+    }
 
 }
