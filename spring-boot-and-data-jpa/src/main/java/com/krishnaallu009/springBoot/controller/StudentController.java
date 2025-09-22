755faf5261650ceb5607710dbad4fc201e517071
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-09-22 20:09:23 +0530

    get All Students

--- spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/controller/StudentController.java
+++ spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.krishnaallu009.springBoot.controller;
 
 import com.krishnaallu009.springBoot.entity.Student;
 import com.krishnaallu009.springBoot.repository.StudentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,10 @@ public class StudentController {
     @PostMapping("/students")
     public Student createStudent(@RequestBody Student student) {
         return studentRepository.save(student);
+    }
+
+    @GetMapping("/students")
+    public java.util.List<Student> getAllStudents() {
+        return studentRepository.findAll();
     }
 }
