ff3a989c2b2ff21663b057fe989a0c060dbc4fa0
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-09-27 16:49:41 +0530

    Add SchoolDto and SchoolResponseDto; update SchoolController to handle DTOs for school creation and retrieval

--- spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/controller/SchoolController.java
+++ spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/controller/SchoolController.java
@@ -1,5 +1,7 @@
 package com.krishnaallu009.springBoot.controller;
 
+import com.krishnaallu009.springBoot.dto.SchoolDto;
+import com.krishnaallu009.springBoot.dto.SchoolResponseDto;
 import com.krishnaallu009.springBoot.entity.School;
 import com.krishnaallu009.springBoot.repository.SchoolRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +20,31 @@ public class SchoolController {
         this.schoolRepository = schoolRepository;
     }
 
+    private School toSchool(SchoolDto schoolDto){
+        School school = new School();
+        school.setName(schoolDto.name());
+        return school;
+    }
+
+    private SchoolResponseDto toSchoolResponseDto(School school){
+        return new SchoolResponseDto(
+                school.getName()
+        );
+    }
+
     @PostMapping("/schools")
-    public School createSchool(@RequestBody School school){
-        return schoolRepository.save(school);
+    public SchoolResponseDto createSchool(@RequestBody SchoolDto schoolDto){
+        var school = toSchool(schoolDto);
+        var savedSchool = schoolRepository.save(school);
+
+        return toSchoolResponseDto(savedSchool);
     }
 
     @GetMapping("/schools")
-    public List<School> getAllSchools() {
-        return schoolRepository.findAll();
+    public List<SchoolResponseDto> getAllSchools() {
+        return schoolRepository.findAll()
+                .stream()
+                .map(this::toSchoolResponseDto)
+                .toList();
     }
 }
