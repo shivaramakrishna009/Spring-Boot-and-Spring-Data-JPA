f02b5c057218c92800f95ccd5da7d3c7441704c9
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-09-28 17:56:53 +0530

    Refactor StudentMapperServiceTest to improve test structure and add mapping assertions

--- spring-boot-and-data-jpa/src/test/java/com/krishnaallu009/springBoot/service/StudentMapperServiceTest.java
+++ spring-boot-and-data-jpa/src/test/java/com/krishnaallu009/springBoot/service/StudentMapperServiceTest.java
@@ -1,38 +1,52 @@
 package com.krishnaallu009.springBoot.service;
 
-import org.junit.jupiter.api.*;
+import com.krishnaallu009.springBoot.dto.StudentDto;
+import com.krishnaallu009.springBoot.entity.School;
+import com.krishnaallu009.springBoot.entity.Student;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class StudentMapperServiceTest {
 
-    @BeforeAll
-    static void beforeAll(){
-        System.out.println("From Before All Method");
-    }
-
-    @AfterAll
-    static void afterAll(){
-        System.out.println("From After All Method");
-    }
+    private StudentMapperService studentMapperService;
 
     @BeforeEach
-    void setUp() {
-        System.out.println("From Before each Method");
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.out.println("From After each Method");
+    void setUp(){
+        studentMapperService = new StudentMapperService();
     }
 
     @Test
-    void testMethod1() {
-        System.out.println("From Test Method 1");
+    public void shouldMapStudentDtoToStudentEntity(){
+        StudentDto studentDto = new StudentDto(
+                "Krishna",
+                "Allu",
+                "krishna@gmail.com",
+                1
+        );
+
+        Student student = studentMapperService.toStudent(studentDto);
+        assertEquals(studentDto.firstName(), student.getFirstName());
+        assertEquals(studentDto.lastName(), student.getLastName());
+        assertEquals(studentDto.email(), student.getEmail());
+        assertNotNull(student.getSchool());
+        assertEquals(studentDto.schoolId(), student.getSchool().getId());
     }
 
     @Test
-    void testMethod2() {
-        System.out.println("From Test Method 2");
+    public void shouldMapStudentEntityToStudentResponseDto() {
+        Student student = new Student();
+        student.setFirstName("Bhargavi");
+        student.setLastName("Ram");
+        student.setEmail("rambha@gmail.com");
+        student.setAge(20);
+
+        var studentResponseDto = studentMapperService.toStudentResponseDto(student);
+        assertEquals(student.getFirstName(), studentResponseDto.firstName());
+        assertEquals(student.getLastName(), studentResponseDto.lastName());
+        assertEquals(student.getEmail(), studentResponseDto.email());
     }
 }
