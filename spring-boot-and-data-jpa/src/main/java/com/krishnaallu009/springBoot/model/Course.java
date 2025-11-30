a8e9863aa1f35725a96d138ff91bb8efbf1812d9
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-11-30 16:11:25 +0530

    Optimized the imports

--- spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/model/Course.java
+++ spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/model/Course.java
@@ -1,7 +1,10 @@
 package com.krishnaallu009.springBoot.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
--- spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/model/Resource.java
+++ spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/model/Resource.java
@@ -1,7 +1,12 @@
 package com.krishnaallu009.springBoot.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
