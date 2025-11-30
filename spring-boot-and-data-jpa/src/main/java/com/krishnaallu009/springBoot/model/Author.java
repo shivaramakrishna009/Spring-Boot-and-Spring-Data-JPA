80b565d030f7a3b3646a4085bc5dd32e7bc13fc7
shivaramakrishna906 <shivaramakrishna906@gmail.com>
2025-11-30 11:10:27 +0530

    Enhance Author entity with additional fields and constraints; update datasource URL in application.yml

--- spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/model/Author.java
+++ spring-boot-and-data-jpa/src/main/java/com/krishnaallu009/springBoot/model/Author.java
@@ -3,6 +3,8 @@ package com.krishnaallu009.springBoot.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -35,12 +37,31 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
 
     private String lastName;
 
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
 
     private int age;
+
+    @Column(
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime updatedAt;
 
 }
